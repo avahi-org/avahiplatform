c6439a2b7d287dd09976143ba6f08f48a2fb0b94
Jon Turdiev <58934993+JonTurdiev@users.noreply.github.com>
2024-10-13 14:33:08 -0700

    Refactor summarizer to use helpers

--- avahiplatform/summarizer.py
+++ avahiplatform/summarizer.py
@@ -1,34 +1,35 @@
 import os
-from .helpers import Helpers
+from helpers import utils
 from loguru import logger
+from helpers.bedrock_helper import BedrockHelper
+
 
 class BedrockSummarizer:
-    def __init__(self, default_model_name='sonnet-3', aws_access_key_id=None,
-                 aws_secret_access_key=None, region_name=None):
-        self.helpers = Helpers(default_model_name=default_model_name,
-                              aws_access_key_id=aws_access_key_id,
-                              aws_secret_access_key=aws_secret_access_key,
-                              region_name=region_name)
+    def __init__(self, bedrock_helper: BedrockHelper):
+        self.bedrock_helper = bedrock_helper
 
     def summarize_text(self, text, user_prompt=None, model_name=None):
         if not text:
             logger.error("Input text cannot be empty.")
             raise ValueError("Input text cannot be empty.")
 
-        prompt = user_prompt if user_prompt else f"Please summarize the following text: {text}"
-        return self.helpers.model_invoke(prompt, model_name)
+        prompt = user_prompt if user_prompt else f"Please summarize the following text: {
+            text}"
+        return self.bedrock_helper.model_invoke(prompt, model_name)
 
     def summarize_file(self, file_path, user_prompt=None, model_name=None):
         if not os.path.exists(file_path):
-            logger.error(f"The file at {file_path} does not exist. Please check the file path.")
-            raise ValueError(f"The file at {file_path} does not exist. Please check the file path.")
+            logger.error(f"The file at {
+                         file_path} does not exist. Please check the file path.")
+            raise ValueError(
+                f"The file at {file_path} does not exist. Please check the file path.")
 
         _, file_extension = os.path.splitext(file_path)
         logger.info(f"Processing file: {file_path}")
         if file_extension.lower() == '.pdf':
-            text = self.helpers.read_pdf(file_path)
+            text = utils.read_pdf(file_path)
         elif file_extension.lower() == '.docx':
-            text = self.helpers.read_docx(file_path)
+            text = utils.read_docx(file_path)
         else:
             with open(file_path, 'r', encoding="utf-8") as file:  # Explicitly setting encoding
                 text = file.read()
@@ -36,5 +37,6 @@ class BedrockSummarizer:
         return self.summarize_text(text, user_prompt, model_name)
 
     def summarize_s3_file(self, s3_file_path, user_prompt=None, model_name=None):
-        text = self.helpers.read_s3_file(s3_file_path=s3_file_path)
+        text = utils.read_s3_file(s3_file_path=s3_file_path)
+
         return self.summarize_text(text, user_prompt, model_name)
